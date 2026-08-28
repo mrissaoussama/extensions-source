b1ba35171406b5232b56301d4e385a6432517718
oussama <mrissaoussama@gmail.com>
2026-08-28 23:05:16 +0100

    fix(novelbincom): use overridden vals instead of removed ReadNovelFull constructor params

--- src/en/novelbincom/src/eu/kanade/tachiyomi/extension/en/novelbincom/NovelBinCom.kt
+++ src/en/novelbincom/src/eu/kanade/tachiyomi/extension/en/novelbincom/NovelBinCom.kt
@@ -2,12 +2,11 @@ package eu.kanade.tachiyomi.novelextension.en.novelbincom
 
 import eu.kanade.tachiyomi.multisrc.readnovelfull.ReadNovelFull
 
-class NovelBinCom :
-    ReadNovelFull(
-        name = "Novel-Bin (com)",
-        baseUrl = "https://novel-bin.com",
-        lang = "en",
-    ) {
+class NovelBinCom : ReadNovelFull() {
+    override val name = "Novel-Bin (com)"
+    override val baseUrl = "https://novel-bin.com"
+    override val lang = "en"
+
     override val popularPage = "monthvisit"
     override val latestPage = "dayvisit"
 
